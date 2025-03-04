a1f4795df1d70a2ede6efe75b43e70dc4a481793
Gael Quirarte Sandoval <up240434@alumnos.upa.edu.mx>
2025-03-04 09:37:26 -0600

    Cambios 4 de Marzo

---
+++ Day 4.py
@@ -0,0 +1,108 @@
+print("           ")
+print("Programa 1.")
+
+print("           ")
+print("Programa 2.")
+
+print("           ")
+print("Programa 3.")
+
+print("           ")
+print("Programa 4.")
+
+print("           ")
+print("Programa 5.")
+
+print("           ")
+print("Programa 6.")
+
+print("           ")
+print("Programa 7.")
+
+print("           ")
+print("Programa 8.")
+
+print("           ")
+print("Programa 9.")
+
+print("           ")
+print("Programa 10.")
+
+print("           ")
+print("Programa 11.")
+
+print("           ")
+print("Programa 12.")
+
+print("           ")
+print("Programa 13.")
+
+print("           ")
+print("Programa 14.")
+
+print("           ")
+print("Programa 15.")
+
+print("           ")
+print("Programa 16.")
+
+print("           ")
+print("Programa 17.")
+
+print("           ")
+print("Programa 18.")
+
+print("           ")
+print("Programa 19.")
+
+print("           ")
+print("Programa 20.")
+
+print("           ")
+print("Programa 21.")
+
+print("           ")
+print("Programa 22.")
+
+print("           ")
+print("Programa 23.")
+
+print("           ")
+print("Programa 24.")
+
+print("           ")
+print("Programa 25.")
+
+print("           ")
+print("Programa 26.")
+
+print("           ")
+print("Programa 27.")
+
+print("           ")
+print("Programa 28.")
+
+print("           ")
+print("Programa 29.")
+
+print("           ")
+print("Programa 30.")
+
+print("           ")
+print("Programa 31.")
+
+print("           ")
+print("Programa 32.")
+
+print("           ")
+print("Programa 33.")
+
+print("           ")
+print("Programa 34.")
+
+print("           ")
+print("Programa 35.")
+
+print("           ")
+print("Programa 36.")
+
