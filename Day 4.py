74de306c2e6f343eca3a04eb7e2238a671d0ad92
Gael Quirarte Sandoval <up240434@alumnos.upa.edu.mx>
2025-03-05 10:34:39 -0600

    Cambios 5 de Marzo

--- Day 4.py
+++ Day 4.py
@@ -1,108 +1,199 @@
+
+#Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
 print("           ")
 print("Programa 1.")
+first = 'Thirty '
+second = 'Days '
+thirty = 'Of '
+fourty = 'Python'
+conca = first +  second + thirty + fourty
+print(conca)
 
+#Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
 print("           ")
 print("Programa 2.")
+first = 'Coding '
+second = 'For '
+thirty = 'All'
+conca = first +  second + thirty 
+print(conca)
 
+#Declare a variable named company and assign it to an initial value "Coding For All".
 print("           ")
 print("Programa 3.")
+company = 'Coding for All'
 
+#Print the variable company using print().
 print("           ")
 print("Programa 4.")
+company = 'Coding for All'
+print (company)
 
+#Print the length of the company string using len() method and print().
 print("           ")
 print("Programa 5.")
+company = 'Coding for All'
+print (company)
+print (len (company))
 
+#Change all the characters to uppercase letters using upper() method.
 print("           ")
 print("Programa 6.")
+company = 'Coding for All'
+company.upper ()
+print (company.upper ())
 
+#Change all the characters to lowercase letters using lower() method.
 print("           ")
 print("Programa 7.")
+company = 'Coding for All'
+company.lower ()
+print (company.lower ())
 
+#Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
 print("           ")
 print("Programa 8.")
+titulo = 'coding for all'
+print(titulo.title())  #Coding For All
+print("           ")
+swap = 'COding For All'
+print(swap.swapcase())  #coDING fOR aLL
+swap2 = 'coding for all'
+print(swap2.swapcase())  #CODING FOR ALL
+print("           ")
+capit = 'Coding for all'
+print(capit.capitalize()) #Coding for all
 
+#Cut(slice) out the first word of Coding For All string
 print("           ")
 print("Programa 9.")
+company = ' Coding for All'
+print (company[7:11] + company [11:15])
 
+#Check if Coding For All string contains a word Coding using the method index, find or other methods.
 print("           ")
 print("Programa 10.")
+f = 'Coding For All'
+print(f.find('Coding'))  # 1-6
 
+#Replace the word coding in the string 'Coding For All' to Python.
 print("           ")
 print("Programa 11.")
+R = 'Coding for all'
+print(R.replace('Coding for all', 'Python')) # 'Python'
 
+
+#Change Python for Everyone to Python for All using the replace method or other methods.
 print("           ")
 print("Programa 12.")
+R = 'Python For Everyone'
+print(R.replace('Python For Everyone', 'Python')) # 'Python'
 
+
+#Split the string 'Coding For All' using space as the separator (split()) .
 print("           ")
 print("Programa 13.")
+C = 'Coding For All'
+print(C.split()) # ['thirty', 'days', 'of', 'python']
 
+
+#"Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
 print("           ")
 print("Programa 14.")
 
+#What is the character at index 0 in the string Coding For All.
 print("           ")
 print("Programa 15.")
 
+#What is the last index of the string Coding For All.
 print("           ")
 print("Programa 16.")
 
+#What character is at index 10 in "Coding For All" string.
 print("           ")
 print("Programa 17.")
 
+#Create an acronym or an abbreviation for the name 'Python For Everyone'.
 print("           ")
 print("Programa 18.")
 
+#Create an acronym or an abbreviation for the name 'Coding For All'.
 print("           ")
 print("Programa 19.")
 
+#Use index to determine the position of the first occurrence of C in Coding For All.
 print("           ")
 print("Programa 20.")
 
+#Use index to determine the position of the first occurrence of F in Coding For All.
 print("           ")
 print("Programa 21.")
 
+#Use rfind to determine the position of the last occurrence of l in Coding For All People.
 print("           ")
 print("Programa 22.")
 
+#Use index or find to find the position of the first occurrence of the word 'because' in the following sentence: 
+#'You cannot end a sentence with because because because is a conjunction'
 print("           ")
 print("Programa 23.")
 
+#Use rindex to find the position of the last occurrence of the word because in the following sentence: 
+#'You cannot end a sentence with because because because is a conjunction'
 print("           ")
 print("Programa 24.")
 
+#Slice out the phrase 'because because because' in the following sentence: 
+# 'You cannot end a sentence with because because because is a conjunction'
 print("           ")
 print("Programa 25.")
 
+#Find the position of the first occurrence of the word 'because' in the following sentence: 
+#'You cannot end a sentence with because because because is a conjunction'
 print("           ")
 print("Programa 26.")
 
+#Slice out the phrase 'because because because' in the following sentence:
+# 'You cannot end a sentence with because because because is a conjunction'
 print("           ")
 print("Programa 27.")
 
+#Does ''Coding For All' start with a substring Coding?
 print("           ")
 print("Programa 28.")
 
+#¿'Coding For All' termina con una subcadena 'coding '?
 print("           ")
 print("Programa 29.")
 
+#'   Coding For All      '  , remove the left and right trailing spaces in the given string.
 print("           ")
 print("Programa 30.")
 
+#Which one of the following variables return True when we use the method isidentifier():
+#30DaysOfPython
+#thirty_days_of_python
 print("           ")
 print("Programa 31.")
 
+#The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon'].
+# Join the list with a hash with space string.
 print("           ")
 print("Programa 32.")
 
+#Use the new line escape sequence to separate the following sentences.
 print("           ")
 print("Programa 33.")
 
+#Use a tab escape sequence to write the following lines.
 print("           ")
 print("Programa 34.")
 
+#Use the string formatting method to display the following:
 print("           ")
 print("Programa 35.")
 
+#Make the following using string formatting methods:
 print("           ")
 print("Programa 36.")
 
