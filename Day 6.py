ae2824139cedafba82adee27dc9605cbea22c15a
Gael Quirarte Sandoval <up240434@alumnos.upa.edu.mx>
2025-03-11 09:36:18 -0600

    Cambios 11 de Febrero

--- Day 6.py
+++ Day 6.py
@@ -5,22 +5,35 @@
 #Create an empty tuple
 print("           ")
 print('Programa 1')
+empty_tuple = tuple()
+print(empty_tuple)
 
 #Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
 print("           ")
 print('Programa 2')
+tupla_hermana = ('Alexa', 'Paola')
+tupla_hermano = ('Hector', 'Jose')
+print(tupla_hermana)
+print(tupla_hermano)
 
 #Join brothers and sisters tuples and assign it to siblings
 print("           ")
 print('Programa 3')
+tupla_hermanos = tupla_hermana + tupla_hermano
+print(tupla_hermanos)
+
 
 #How many siblings do you have?
 print("           ")
 print('Programa 4')
+print('Tengo: ',len(tupla_hermanos), ' hermanos')
 
 #Modify the siblings tuple and add the name of your father and mother and assign it to family_members
 print("           ")
 print('Programa 5')
+tupla_papas = ('Florencio', 'Maria')
+tupla_family_members = tupla_hermana + tupla_hermano + tupla_papas
+print(tupla_family_members)
 
 
 
@@ -34,39 +47,66 @@ print("           ")
 #Unpack siblings and parents from family_members
 print("           ")
 print('Programa 2.1')
+tupla_hermana = ('Alexa', 'Paola')
+tupla_hermano = ('Hector', 'Jose')
+tupla_papas = ('Florencio', 'Maria')
+tupla_family_members = tupla_hermana + tupla_hermano + tupla_papas
+del tupla_family_members
+
 
 #Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
 print("           ")
 print('Programa 2.2')
+frutas = ('Banana', 'Orange', 'Mango',)
+vegetales = ('Tomato', 'Potato', 'Cabbage')
+animales = ('Egg', 'Meat', 'Milk')
+food_stuff_tp = frutas + vegetales + animales
+print(food_stuff_tp)
 
 #Change the about food_stuff_tp tuple to a food_stuff_lt list
 print("           ")
 print('Programa 2.3')
+food_stuff_lt = list(food_stuff_tp)
+print (food_stuff_lt)
 
 #Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
 print("           ")
 print('Programa 2.4')
+enmedio_food= food_stuff_lt[4]
+print(enmedio_food)  
 
 #Slice out the first three items and the last three items from food_staff_lt list
 print("           ")
 print('Programa 2.5')
+print(food_stuff_lt)
+print("           ")
+First_food = food_stuff_lt[0:3]
+print(First_food)
+print("           ")
+Last_food = food_stuff_lt[-3:]
+print(Last_food)
+print("           ")
+Ultimate_food = food_stuff_lt[3:6]
+print(Ultimate_food)
+
 
 #Delete the food_staff_tp tuple completely
 print("           ")
 print('Programa 2.6')
+print(food_stuff_tp)
+del food_stuff_tp
 
-#Check if an item exists in tuple:
-print("           ")
-print('Programa 2.7')
 
 #Check if 'Estonia' is a nordic country
 print("           ")
-print('Programa 2.8')
+print('Programa 2.7')
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print('Estonia' in nordic_countries)
 
 #Check if 'Iceland' is a nordic country
 print("           ")
-print('Programa 2.9')
+print('Programa 2.8')
+print('Iceland' in nordic_countries)
+print("           ")
 
 
-
-#nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
---
+++ Day 7.py
@@ -0,0 +1,91 @@
+# EXERCISES: LEVEL 1.
+
+#Find the length of the set it_companies
+print("           ")
+print('Programa 1')
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+print(len(it_companies))
+
+
+#Add 'Twitter' to it_companies
+print("           ")
+print('Programa 2')
+it_companies.add('Twitter')
+print (it_companies)
+
+
+#Insert multiple IT companies at once to the set it_companies
+print("           ")
+print('Programa 3')
+other_ti = {'Instagram', 'Youtube', 'WhatsApp'}
+print(it_companies.union(other_ti))
+
+
+#Remove one of the companies from the set it_companies
+print("           ")
+print('Programa 4')
+
+
+
+#What is the difference between remove and discard
+print("           ")
+print('Programa 5')
+
+
+# EXERCISES: LEVEL 2.
+
+
+#Join A and B
+print("           ")
+print('Programa 2.1')
+
+
+#Find A intersection B
+print("           ")
+print('Programa 2.2')
+
+
+#Is A subset of B
+print("           ")
+print('Programa 2.3')
+
+
+#Are A and B disjoint sets
+print("           ")
+print('Programa 2.4')
+
+
+#Join A with B and B with A
+print("           ")
+print('Programa 2.5')
+
+
+#What is the symmetric difference between A and B
+print("           ")
+print('Programa 2.6')
+
+
+#Delete the sets completely
+print("           ")
+print('Programa 2.7')
+
+
+
+# EXERCISES: LEVEL 3.
+
+
+#Convert the ages to a set and compare the length of the list and the set, which one is bigger?
+print("           ")
+print('Programa 3.1')
+
+
+#Explain the difference between the following data types: string, list, tuple and set
+print("           ")
+print('Programa 3.2')
+
+
+#I am a teacher and I love to inspire and teach people. How many unique words have been used in the sentence? 
+# Use the split methods and set to get the unique words.
+print("           ")
+print('Programa 3.3')
+
