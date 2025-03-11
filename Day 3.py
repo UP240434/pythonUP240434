96d044e327e3aff2b10f6e98d003482b43fedf37
Gael Quirarte Sandoval <up240434@alumnos.upa.edu.mx>
2025-03-10 18:58:53 -0600

    Ultimo Cambio dia 3

--- Day 3.py
+++ Day 3.py
@@ -1,6 +1,7 @@
 
 #Program 1
 #Declare your age as integer variable
+print("           ")
 print('Programa 1')
 Edad = int(input("Ingresa tu edad: "))
 print ("Tu edad es:", " ",Edad) 
@@ -8,6 +9,7 @@ print(type(int(Edad)))
 
 #Program 2
 #Declare your height as a float variable
+print("           ")
 print('Programa 2')
 Edad = float(input("Ingresa tu edad: "))
 print ("Tu edad es:", " ",Edad) 
@@ -15,6 +17,7 @@ print(type(float(Edad)))
 
 #Program 3
 #Declare a variable that store a complex number
+print("           ")
 print('Programa 3')
 complex = float(input("Ingresa tu numero: "))
 print('Complex number: ', 1 + 1j)
@@ -23,6 +26,7 @@ print('Complex number: ', 1 + 1j)
 #Wrote a script that prompts the user to enter base and height
 #Of the triangle and calculate an area of this triangle.
 # (area = 0.5 x b x h)
+print("           ")
 print('Programa 4')
 base = float (input("Ingresa el valor de la base: "))
 altura = float (input ("Ingresa el valor de la altura: "))
@@ -33,6 +37,7 @@ print ("El area del triangulo es", " ",area)
 #Write a script that prompts the user to enter side a, side b, and side c of the triangle. 
 #Calculate the perimeter of the triangle 
 #(perimeter = a + b + c).
+print("           ")
 print('Programa 5')
 a = float(input("Ingresa el valor del lado a: "))
 b = float(input("Ingresa el valor del lado b: "))
@@ -44,6 +49,7 @@ print("El perimetro del triangulo es:", " ",perimeter)
 #Get length and width of a rectangle using prompt.
 # Calculate its area (area = length x width) 
 # and perimeter (perimeter = 2 x (length + width))
+print("           ")
 print('Programa 6')
 lenght = float(input("Ingresa el largo del rectangulo: "))
 width = float(input("Ingresa el ancho del rectangulo: "))
@@ -56,6 +62,7 @@ print("El perimetro del rectangulo es:"," ",perimeter)
 #Get radius of a circle using prompt. 
 # Calculate the area (area = pi x r x r) 
 # and circumference (c = 2 x pi x r) where pi = 3.14.
+print("           ")
 print('Programa 7')
 pi = 3.14
 r = float(input("Ingresa el valor del radio: "))
@@ -68,6 +75,7 @@ print("La circunferencia del circulo es:"," ",c)
 # Calculate the slope, 
 # x-intercept 
 # and y-intercept of y = 2x -2
+print("           ")
 print('Programa 8')
 x= float(input("Ingresa el valor de x: "))
 y = 2 * x - 2
@@ -77,6 +85,7 @@ print("El valor de y es:"," ",y)
 # Slope is (m = y2-y1/x2-x1).
 # Find the slope and Euclidean distance between point (2, 2) 
 # and point (6,10)
+print("           ")
 print('Programa 9')
 x1 = 2
 x2 = 6
@@ -87,6 +96,7 @@ print("El valor de m es:"," ",m)
 
 #Program 10
 #Compare the slopes in tasks 8 and 9.
+print("           ")
 print('Programa 10')
 x = 0
 yInt = 2*x + 2
@@ -107,6 +117,7 @@ else:
 
 #Program 11
 #Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is going to be 0.
+print("           ")
 print('Programa 11')
 x= float(input("Ingresa el valor de x: "))
 y =( (x**2)+(6*x)+9)
@@ -118,6 +129,7 @@ else:
 
 #Program 12
 #Find the length of 'python' and 'dragon' and make a falsy comparison statement.
+print("           ")
 print('Programa 12')
 pyhton = ("python")
 dragon = ("dragon")
@@ -127,6 +139,7 @@ print(len('python') > len('dragon'))   # False
 
 #Program 13
 #Use and operator to check if 'on' is found in both 'python' and 'dragon'
+print("           ")
 print('Programa 13')
 pyhton = ("python")
 dragon = ("dragon")
@@ -134,11 +147,13 @@ print ("on in python and dragon", "on" in "python and dragon"," ")
 
 #Program 14
 #I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+print("           ")
 print('Programa 14')
 print ("jargon in I hope this course us not full of jargon", "jargon" in "I hope this course us not full of jargon"," ")
 
 #Program 15
 #There is no 'on' in both dragon and python
+print("           ")
 print('Programa 15')
 print ("On in python ", "On" in "python "," ")
 print ("On in dragon ", "On" in "dragon"," ")
@@ -146,6 +161,7 @@ print ("On in dragon ", "On" in "dragon"," ")
 #Program 16
 #Find the length of the text python and convert the value to float 
 # and convert it to string
+print("           ")
 print('Programa 16')
 pyhton = ("python")
 longTexto = len(pyhton)
@@ -156,6 +172,7 @@ print (type(strlong))
 
 #Program 17
 #Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+print("           ")
 print('Programa 17')
 x= int(input("Ingresa el valor de x: "))
 print (x)
@@ -166,6 +183,7 @@ else:
 
 #Program 18
 #Check if the floor division of 7 by 3 is equal to the int converted value of 2.7.
+print("           ")
 print('Programa 18')
 division = (7/3)
 print (division)
@@ -178,6 +196,7 @@ else:
 
 #Program 19
 #Check if type of '10' is equal to type of 10
+print("           ")
 print('Programa 19')
 ten = 10
 if (type (ten) == int):
@@ -188,6 +207,7 @@ else:
 
 #Program 20
 #Check if int('9.8') is equal to 10
+print("           ")
 print('Programa 20')
 nine = 9.8
 entero =int(nine)
@@ -199,6 +219,7 @@ else:
 
 #Program 21
 #Writ a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+print("           ")
 print('Programa 21')
 h = float(input("Ingresa tus horas horas: "))
 r = float(input("Ingresa la tasa por hora: "))
@@ -209,6 +230,7 @@ print("Tu salario en total es de:", " ",s)
 #Write a script that prompts the user to enter number of years.
 #Calculate the number of seconds a person can live. 
 # Assume a person can live hundred years
+print("           ")
 print('Programa 22')
 a = float(input("Ingresa tu numero de años: "))
 s1 = (3600*24)
@@ -218,6 +240,7 @@ print("los segundos totales que has vivido son:", " ",s3)
 
 #Programa 23
 #Write a Python script that displays the following table
+print("           ")
 print('Programa 23')
 print (1, int(1/1), 1*1, 1**2, 1**3)
 print ("---------")
---
+++ Day 5.py
@@ -0,0 +1,133 @@
+#Declare an empty list
+print("           ")
+print('Programa 1')
+
+
+#Declare a list with more than 5 items
+print("           ")
+print('Programa 2')
+
+
+#Find the length of your list
+print("           ")
+print('Programa 3')
+
+
+#Get the first item, the middle item and the last item of the list
+print("           ")
+print('Programa 4')
+
+
+#Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+print("           ")
+print('Programa 5')
+
+
+#Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+print("           ")
+print('Programa 6')
+
+
+#Print the list using print()
+print("           ")
+print('Programa 7')
+
+
+#Print the number of companies in the list
+print("           ")
+print('Programa 8')
+
+
+#Print the first, middle and last company
+print("           ")
+print('Programa 9')
+
+
+#Print the list after modifying one of the companies
+print("           ")
+print('Programa 10')
+
+
+#Add an IT company to it_companies
+print("           ")
+print('Programa 11')
+
+
+#Insert an IT company in the middle of the companies list
+print("           ")
+print('Programa 12')
+
+
+#Change one of the it_companies names to uppercase (IBM excluded!)
+print("           ")
+print('Programa 13')
+
+
+#Join the it_companies with a string '#;  '
+print("           ")
+print('Programa 14')
+
+
+#Check if a certain company exists in the it_companies list
+print("           ")
+print('Programa 15')
+
+
+#
+print("           ")
+print('Programa 16')
+
+
+#
+print("           ")
+print('Programa 17')
+
+
+#
+print("           ")
+print('Programa 18')
+
+
+#
+print("           ")
+print('Programa 19')
+
+
+#
+print("           ")
+print('Programa 20')
+
+
+#
+print("           ")
+print('Programa 21')
+
+
+#
+print("           ")
+print('Programa 22')
+
+
+#
+print("           ")
+print('Programa 23')
+
+
+#
+print("           ")
+print('Programa 24')
+
+
+#
+print("           ")
+print('Programa 25')
+
+
+#
+print("           ")
+print('Programa 26')
+
+
+#
+print("           ")
+print('Programa 27')
