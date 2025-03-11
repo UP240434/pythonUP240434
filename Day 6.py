f8babddaad258000dd0290d902f3be3a9c3e73ad
Gael Quirarte Sandoval <up240434@alumnos.upa.edu.mx>
2025-03-10 22:22:18 -0600

    Cambios 10 de Marzo

---
+++ Day 6.py
@@ -0,0 +1,72 @@
+# EXERCISES: LEVEL 1.
+
+
+
+#Create an empty tuple
+print("           ")
+print('Programa 1')
+
+#Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
+print("           ")
+print('Programa 2')
+
+#Join brothers and sisters tuples and assign it to siblings
+print("           ")
+print('Programa 3')
+
+#How many siblings do you have?
+print("           ")
+print('Programa 4')
+
+#Modify the siblings tuple and add the name of your father and mother and assign it to family_members
+print("           ")
+print('Programa 5')
+
+
+
+
+
+print("           ")
+# EXERCISES: LEVEL 2.
+
+
+
+#Unpack siblings and parents from family_members
+print("           ")
+print('Programa 2.1')
+
+#Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+print("           ")
+print('Programa 2.2')
+
+#Change the about food_stuff_tp tuple to a food_stuff_lt list
+print("           ")
+print('Programa 2.3')
+
+#Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+print("           ")
+print('Programa 2.4')
+
+#Slice out the first three items and the last three items from food_staff_lt list
+print("           ")
+print('Programa 2.5')
+
+#Delete the food_staff_tp tuple completely
+print("           ")
+print('Programa 2.6')
+
+#Check if an item exists in tuple:
+print("           ")
+print('Programa 2.7')
+
+#Check if 'Estonia' is a nordic country
+print("           ")
+print('Programa 2.8')
+
+#Check if 'Iceland' is a nordic country
+print("           ")
+print('Programa 2.9')
+
+
+
+#nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
