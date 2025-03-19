0805f9022975ea60b961e75c7862a5d8be46ae50
Gael Quirarte Sandoval <up240434@alumnos.upa.edu.mx>
2025-03-19 10:41:21 -0600

    Cambios 19 de marzo

--- 09_Day/Day 9.py
+++ 09_Day/Day 9.py
@@ -1,3 +1,5 @@
+
+
 # Ejercicios: Nivel 1
 
 #Get user input using input(“Enter your age: ”). 
@@ -35,46 +37,79 @@ else:
         print('Mi edad es mayor por:', dif , ' años')
 
         
-Obtener dos números del usuario mediante la solicitud de entrada. Si a es mayor que b, devuelve a mayor que b; si a es menor que b, devuelve a menor que b; de lo contrario,
-a es igual a b. Salida:
-
-Enter number one: 4
-Enter number two: 3
-4 is greater than 3
-
 #Get two numbers from the user using input prompt. If a is greater than b return a is greater than b, if a is less b return a is smaller than b, else a is equal to b. Output:
 print("           ")
 print('Programa 3')
-num1 = float(input("Ingresa el primer numero:"))
-num2 = float(input("Ingresa el segundo numero:"))
+
+num1 = float(input("Ingresa el primer numero (a):"))
+num2 = float(input("Ingresa el segundo numero (b):"))
+
 if num1 > num2:
-    if dif == 1 or dif2 == 1:
-        print('La diferencia es de 1 año')
+    if num1 > num2:
+        print('a es mayor que b')
     else:
-        print('')
-elif Myage == your_age:
-    print('La edad es igual')
-else:
-    if Myage < your_age:
-        print('Tu edad es mayor por:', dif2 , 'años.')
-    else:
-        print('Mi edad es mayor por:', dif , ' años')
+        print('a es menor que b')
+elif num1 == num2:
+         print('a y b son iguales')
+elif num1 < num2:
+    print ('a es menor que b')
+
+    
 
 # Ejercicios: Nivel 2
 
 #Write a code which gives grade to students according to theirs scores:
 print("           ")
 print('Programa 2.1')
+Cal = float(input("Ingresa la calificacion:"))
+if Cal >= 90 and Cal <=100:
+    if Cal >= 90 and Cal <=100:
+        print('Tu nota es: A')
+elif Cal >= 70 and Cal <=89:
+         print('Tu nota es: B')
+elif Cal >= 60 and Cal <=69:
+    print ('Tu nota es: C')
+elif Cal >= 50 and Cal <=59:
+    print ('Tu nota es: D')
+elif Cal >= 0 and Cal <=49:
+    print ('Tu nota es: F ')
+
+
 
 #Check if the season is Autumn, Winter, Spring or Summer. 
 # If the user input is: September, October or November, the season is Autumn. December, January or February, the season is Winter. March, April or May,
 # the season is Spring June, July or August, the season is Summer
 print("           ")
 print('Programa 2.2')
+Estacion = str(input("Ingresa el mes:"))
+Estacion2 = (Estacion.title()) 
+if Estacion2 == "Septiembre" or Estacion2 == " Octubre" or Estacion2 == "Noviembre":
+    if Estacion2== "Septiembre" or Estacion2 == " Octubre" or Estacion2 == "Noviembre":
+        print('La estacion es Otoño')
+elif Estacion2 == "Diciembre" or Estacion2 == " Enero" or Estacion2 == "Febrero":
+         print('La estacion es Invierno')
+elif Estacion2 == "Marzo" or Estacion2 == "Abril" or Estacion2 == "Mayo":
+         print('La estacion es Primavera')
+elif Estacion2 == "Junio" or Estacion2 == "Julio" or Estacion2 == "Agosto":
+         print('La estacion es Verano')
+
 
 #The following list contains some fruits:
 print("           ")
 print('Programa 2.3')
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruta = str(input("Ingresa la fruta:")) 
+does_exist = fruta in fruits 
+print(does_exist)
+frutaf = fruits.append(fruta)
+if does_exist == True:
+    if does_exist == True:
+        print('Ya esta la fruta en la lista')
+elif does_exist == False:
+     print(fruits)
+
+
+
 
 # Ejercicios: Nivel 3
 
@@ -91,3 +126,26 @@ print('Programa 2.3')
 #* If the person is married and if he lives in Finland, print the information in the following format:
 print("           ")
 print('Programa 3')
+person={
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_marred': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+    }
+comprobacion = ('skills' in person)
+if comprobacion== True:
+    if comprobacion== True:
+     print(person['skills'][2])
+elif 
+
+
+* Verificar si el diccionario de personas tiene la clave de habilidades; de ser así, imprimir la habilidad central en la lista de habilidades.
+* Verificar si el diccionario de personas tiene la clave de habilidades; de ser así, verificar si la persona tiene la habilidad "Python" e imprimir el resultado.
+* Si una persona solo tiene JavaScript y React, imprimir('Es desarrollador front-end'); si tiene Node, Python y MongoDB, imprimir('Es desarrollador back-end'); si tiene React, Node y MongoDB, imprimir('Es desarrollador full-stack'); de lo contrario, imprimir('Título desconocido'). Para obtener resultados más precisos, se pueden anidar más condiciones.
+* Si la persona está casada y reside en Finlandia, imprimir la información en el siguiente formato:
