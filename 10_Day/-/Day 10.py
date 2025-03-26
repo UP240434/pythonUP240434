e5dda8a7dfd49abb4d9faa6f755eb1016a0d3c98
Gael Quirarte Sandoval <up240434@alumnos.upa.edu.mx>
2025-03-26 08:45:34 -0600

    Ultimos cambios dia 10

---
+++ 10_Day/-/Day 10.py
@@ -0,0 +1,143 @@
+
+#Exercises: Level 1
+
+
+#Iterate 0 to 10 using for loop, do the same using while loop.
+print("           ")
+print('Programa 1')
+count = 0
+while count < 10:
+    print(count)
+    count = count + 1
+else:
+    print(count)
+
+print("           ")
+numbers = (0, 1, 2, 3, 4, 5,6,7,8,9, 10)
+for number in numbers:
+    print(number)
+
+#Iterate 10 to 0 using for loop, do the same using while loop.
+print("           ")
+print('Programa 2')
+count = 10
+while count > 0:
+    print(count)
+    count = count - 1
+else:
+    print(count)
+    
+print("           ")
+numbers = (10, 9, 8, 7, 6, 5,4,3,2,1,0)
+for number in numbers:
+    print(number)
+
+#Write a loop that 
+print("           ")
+print('Programa 3')
+for i in range (0,8):
+    print('#' * i)    
+
+
+#Use nested loops to create the following:
+print("           ")
+print('Programa 4')
+lado = 8
+for i in range(lado):
+    for j in range(lado):
+        print("# ", end="")
+    print()
+
+#Print the following pattern:
+print("           ")
+print('Programa 5')
+for n in [0,1,2,3,4,5,6,7,8,9,10]:
+  cuadrado = n**2
+  print(n, '*', n ,'=' ,cuadrado)
+
+
+#Iterate through the list, ['Python', 'Numpy','Pandas','Django', 'Flask'] using a for loop and print out the items.
+print("           ")
+print('Programa 6')
+programs = ['Python', 'Numpy','Pandas','Django', 'Flask']
+for program in programs: # number is temporary name to refer to the list's items, valid only inside this loop
+    print(program)
+
+#Use for loop to iterate from 0 to 100 and print only even numbers
+print("           ")
+print('Programa 7')
+for number in range(0,100,2):
+    print(number)
+else:
+    print(number + 2)
+
+#Use for loop to iterate from 0 to 100 and print only odd numbers
+print("           ")
+print('Programa 8')
+for number in range(1,100,2):
+    print(number)
+
+
+
+#Exercises: Level 2
+
+#Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+print("           ")
+print('Programa 2.1')
+suma = 0
+for n in range(0,101):
+  suma = suma + n
+print('La suma es:', suma,)
+
+#Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+print("           ")
+print('Programa 2.2')
+suma = 0
+for n in range(0,102,2):
+  suma = suma + n
+print('La suma par es:', suma)
+suma = 0
+for n in range(1,100,2):
+  suma = suma + n
+print('La suma impar es:', suma)
+
+
+
+
+
+#Exercises: Level 3
+
+#Go to the data folder and use the countries.py file. Loop through the countries and extract all the countries containing the word land.
+print("           ")
+print('Programa 3.1')
+import paises as p
+
+paises = p.countries
+
+
+
+#This is a fruit list, ['banana', 'orange', 'mango', 'lemon'] reverse the order using loop.
+print("           ")
+print('Programa 3.2')
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.reverse()
+for fruit in fruits:
+    print(fruit)
+
+
+#Go to the data folder and use the countries_data.py file.
+
+#What are the total number of languages in the data
+#Find the ten most spoken languages from the data
+#Find the 10 most populated countries in the world
+print("           ")
+print('Programa 3.3')
+
+import countries as p
+
+ciudades = p.ciudades
+acum = 0
+
+for ciudad in ciudades:
+    acum = ciudad['population'] + acum
+print ('Somos:', acum)
---
+++ 11_Day/__pycache__/Day 11.py
@@ -0,0 +1,136 @@
+def generate_groups (team,*args):
+    print(team)
+    for i in args:
+        print(i)
+print(generate_groups('Team-1','Asabeneh','Brook','David','Eyob'))
+#Ejercicios: Nivel 1
+
+#Declare la función add_two_numbers . Esta función acepta dos parámetros y devuelve una suma.
+print("           ")
+print('Programa 1')
+def add_two_numbers ():
+    num_one = 2
+    num_two = 3
+    total = num_one + num_two
+    print(total)
+add_two_numbers()
+
+#El área de un círculo se calcula de la siguiente manera: área = π x r x r. Escribe una función que calcule área_del_círculo .
+print("           ")
+print('Programa 2')
+def area_of_circle (r):
+    PI = 3.14
+    area = PI * r ** 2
+    return area
+print(area_of_circle(10))
+
+#Escriba una función llamada add_all_nums que tome cualquier número de argumentos y los sume. Compruebe si todos los elementos de la lista son de tipo numérico. 
+# De no ser así, proporcione una respuesta razonable.
+print("           ")
+print('Programa 3')
+def sum_all_nums(*nums):
+    total = 0
+    for num in nums:
+        total += num    
+    return total
+print(sum_all_nums(2, 3, 5)) 
+
+#La temperatura en °C se puede convertir a °F usando esta fórmula: °F = (°C x 9/5) + 32. Escriba una función que convierta °C a °F, convert_celsius_to-fahrenheit .
+print("           ")
+print('Programa 4')
+def add_Celsius ():
+    c = 45
+    total = (c * 9/5) + 32
+    print(total, " Grados Farenheit")
+add_Celsius()
+
+#Escriba una función llamada check-season, toma un parámetro de mes y devuelve la temporada: Otoño, Invierno, Primavera o Verano.
+print("           ")
+print('Programa 5')
+def check_season (Otono == 'Septiembre' or 'Octubre' or 'Diciembre')
+    age = "Septiembre"
+    return age
+print('Estacion: ', check_season (Otono == 'Septiembre' or 'Octubre' or 'Diciembre'))
+
+    
+#Escriba una función llamada calculate_slope que devuelva la pendiente de una ecuación lineal
+print("           ")
+print('Programa 6')
+
+#La ecuación cuadrática se calcula de la siguiente manera: ax² + bx + c = 0. Escriba una función que calcule el conjunto solución de una ecuación cuadrática,
+# solve_quadratic_eqn .
+print("           ")
+print('Programa 7')
+
+#Declara una función llamada print_list. Esta toma una lista como parámetro e imprime cada elemento de la lista.4print("           ")
+print("           ")
+print('Programa 8')
+
+#Declare una función llamada reverse_list. Esta recibe un array como parámetro y devuelve su valor inverso (usando bucles).
+print("           ")
+print('Programa 9')
+
+#Declare una función llamada capitalize_list_items. Esta toma una lista como parámetro y devuelve una lista de elementos en mayúsculas.
+print("           ")
+print('Programa 10')
+
+#Declara una función llamada add_item. Esta toma como parámetros una lista y un elemento. Devuelve una lista con el elemento añadido al final.
+print("           ")
+print('Programa 11')
+
+#Declara una función llamada remove_item. Esta toma como parámetros una lista y un elemento. Devuelve una lista con el elemento eliminado.
+print("           ")
+print('Programa 12')
+
+#Declara una función llamada suma_de_números. Esta función toma un parámetro numérico y suma todos los números en ese rango.
+print("           ")
+print('Programa 13')
+
+#Declara una función llamada suma_de_impares. Esta función toma un parámetro numérico y suma todos los números impares en ese rango.
+print("           ")
+print('Programa 14')
+
+#Declara una función llamada suma_de_números_pares. Esta función toma un parámetro numérico y suma todos los números pares en ese rango.
+print("           ")
+print('Programa 15')
+
+
+
+#Ejercicios: Nivel 2
+
+#Declare una función llamada evens_and_odds. Esta función toma un entero positivo como parámetro y cuenta el número de pares e impares en el número.
+print("           ")
+print('Programa 2.1')
+#Llama a tu función factorial, toma un número entero como parámetro y devuelve un factorial del número.
+print("           ")
+print('Programa 2.2')
+#Llama a tu función is_empty , toma un parámetro y verifica si está vacío o no
+print("           ")
+print('Programa 2.3')
+
+#Escriba diferentes funciones que acepten listas. Estas funciones deben calcular la media, la mediana, la moda, el rango, la varianza y la desviación estándar (desviación estándar).
+print("           ")
+print('Programa 2.4')
+
+
+
+
+#Ejercicios: Nivel 3
+
+#Escriba una función llamada is_prime, que verifique si un número es primo.
+print("           ")
+print('Programa 3.1')
+
+#Escriba una función que verifique si todos los elementos son únicos en la lista.
+print("           ")
+print('Programa 3.2')
+#Escriba una función que verifique si todos los elementos de la lista son del mismo tipo de datos.
+print("           ")
+print('Programa 3.3')
+#Escriba una función que verifique si la variable proporcionada es una variable de Python válida
+print("           ")
+print('Programa 3.4')
+#Vaya a la carpeta de datos y acceda al archivo Countries-data.py.
+##Crea una función llamada "los países más poblados". Debería devolver los 10 o 20 países más poblados en orden descendente.
+print("           ")
+print('Programa 3.5')
